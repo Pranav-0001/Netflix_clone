534874870b8391977b00bb27dc6a624c0b2c1c16
Pranav-0001 <pranavchakkalakkal@gmail.com>
2023-05-08 12:51:59 +0530

    Added new category

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from "./Components/Navbar/Navbar";
 import './App.css'
 import Banner from "./Components/Banner/Banner";
 import RowPost from "./Components/RowPost/RowPost";
-import {Trending,action,romance,Adventure} from './urls'
+import {Trending,action,romance,Adventure,malayalam} from './urls'
 
 function App() {
   return (
@@ -15,6 +15,7 @@ function App() {
       <RowPost url={action} title="Action" isSmall/>
       <RowPost url={romance} title="Romance" isSmall/>
       <RowPost url={Adventure} title="Adventure" isSmall/>
+      <RowPost url={malayalam} title="Malayalam" isSmall/>
 
     </div>
   );
--- src/urls.js
+++ src/urls.js
@@ -4,4 +4,6 @@ export const Trending=`trending/all/day?api_key=${API_KEY}`
 export const action=`discover/movie?api_key=${API_KEY}&language=en-US&with_genres=28`
 export const romance=`discover/movie?api_key=${API_KEY}&language=en-US&with_genres=10749`
 export const Adventure=`discover/movie?api_key=${API_KEY}&language=en-US&with_genres=12`
+export const malayalam=`discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_original_language=ml`
+
 
